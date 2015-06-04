4c60035994f048c2cd9bac72926e68a3e5656fcd
Espressomilk <jack.shi2013@gmail.com>
2015-06-04 22:36:37 +0800

    Request controller added

---
+++ application/controllers/Request.php
@@ -0,0 +1,24 @@
+<?php
+
+class Request extends CI_Controller {
+	function __construct(){
+    	parent::__construct();
+        $this->load->database();
+        $this->load->library('form_validation');
+        $this->load->library('session');
+    }
+
+    function download() {
+    	$playerId = $this->session->userdata('playerId');
+    	if($playerId == NULL) {
+    		echo -1;
+    		return;
+    	}
+    	$player = $this->db->get_where('userproperty', array('playerId' => $playerId))->row();
+    	$resourceBase = $this->db->get_where('resourcebase');
+    	$counstruction = $this->db->get_where('construction');
+    	echo "playerInfo: ", json_encode($player);
+    	echo "resourceBase: ", json_encode($resourceBase);
+    	echo "construction: ", json_encode($construction);
+    }
+}
