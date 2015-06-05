0d5621af89ef1db3e67e1e43dadf4f125dc7eb95
Espressomilk <jack.shi2013@gmail.com>
2015-06-05 21:14:57 +0800

    a fix

--- application/controllers/Request.php
+++ application/controllers/Request.php
@@ -74,7 +74,7 @@ class Request extends CI_Controller {
     	}
     	else if($targetType == 2) {
     		$detail = $this->construction->get_by_id($targetId);
-    		$username = $this->mpropery->get_username($detail->playerId);
+    		$username = $this->mproperty->get_username($detail->playerId);
     		$res = array(
     					'id'=>$detail->id, 'playerId'=>$detail->playerId, 'username'=>$username, 'location'=>$detail->location,
     					'value'=>$detail->value, 'maxdurability'=>$detail->maxdurability
