c2c582ade78162a002737a5119192ceb4a31ebeb
Harry <harry.t.chao@gmail.com>
2015-07-03 12:30:36 +0800

    add qrcode

---
+++ application/controllers/Qrcode.php
@@ -0,0 +1,12 @@
+<?php
+
+class Qrcode extends CI_CONTROLLER {
+	public function __construct()
+    {
+            parent::__construct();
+            $this->load->helper('url');
+    }
+	function index(){
+		echo "<img src='".base_url()."RealCraft.png' />";
+	}
+}
