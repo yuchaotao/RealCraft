f1f4d22c76e3e33c4c6c338efdf19f4c91502124
Harry <harry.t.chao@gmail.com>
2015-07-03 12:36:02 +0800

    move png to the image folder

--- application/controllers/Qrcode.php
+++ application/controllers/Qrcode.php
@@ -7,6 +7,6 @@ class Qrcode extends CI_CONTROLLER {
             $this->load->helper('url');
     }
 	function index(){
-		echo "<img src='".base_url()."RealCraft.png' />";
+		echo "<img src='".base_url()."image\RealCraft.png' />";
 	}
 }
